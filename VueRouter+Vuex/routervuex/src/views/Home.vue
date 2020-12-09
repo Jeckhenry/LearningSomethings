98c5624e4fda4911ddf447ff766629c58e32354c
571977485 <571977485@qq.com>
2020-12-09 13:44:29 +0800

    feat

--- VueRouter+Vuex/routervuex/src/views/Home.vue
+++ VueRouter+Vuex/routervuex/src/views/Home.vue
@@ -5,6 +5,8 @@
     <div>$store.getters.doubleCount: {{ $store.getters.doubleCount }}</div>
     <div><button @click="$store.commit('add')">commit mutation</button></div>
     <div><button @click="$store.dispatch('add')">dispatch acation</button></div>
+    {{ obj.b }}
+    <button @click="add">修改</button>
   </div>
 </template>
 
@@ -12,5 +14,18 @@
 // @ is an alias to /src
 export default {
   name: "Home",
+  data() {
+    return {
+      obj: {
+        a: 1,
+        b: [1, 2, 3],
+      },
+    };
+  },
+  methods: {
+    add() {
+      this.obj.b = ["a"];
+    },
+  },
 };
 </script>
--- Vue相关/Vue源码练习/defineReactive/index.js
+++ Vue相关/Vue源码练习/defineReactive/index.js
@@ -1,15 +1,16 @@
 const obj = {}
 
 function update() {
-    app.innerHTML += new Date().toLocaleTimeString();
+    app.innerHTML = new Date().toLocaleTimeString();
 }
+
 function defineReactive(obj, key, val) {
     Object.defineProperty(obj, key, {
         get() {
             console.log(`getting: ${key} + ${val}`);
             return val;
         },
-        set(newVal) {   
+        set(newVal) {
             if (typeof newVal === 'object') {
                 observe(newVal);
             }
@@ -29,9 +30,16 @@ function observe(obj) {
     Object.keys(obj).forEach((key) => {
         if (typeof obj[key] === 'object') {
             observe(obj[key]);
+            if (Object.prototype.toString.call(obj[key]) === '[object Array]') {
+
+            }
         }
         defineReactive(obj, key, obj[key]);
     });
+}
+
+function set(obj, key, val) {
+    defineReactive(obj, key, val);
 }
 
 // defineReactive(obj, 'foo', 'foo');
@@ -55,7 +63,8 @@ const obj2 = {
     classInfo: {
         math: 'math',
         eng: 'eng',
-    }
+    },
+    arr: [1, 2, 3],
 };
 observe(obj2);
 obj2.name;
@@ -66,6 +75,12 @@ obj2.name = {
 };
 obj2.name.ll;
 
-setInterval(() => {
-    obj2.name.ll = 'aa';
-}, 1000);
+// set(obj2, 'lll', 123);
+// obj2.lll
+// obj2.lll = 1232
+
+obj2.arr
+
+// setInterval(() => {
+// obj2.name.ll = 'aa';
+// }, 1000);
